5bfb3a2baa074f83729b375ab48dc0fe4ffeebc7
Daniel Ellis <danielshawellis@gmail.com>
2020-04-02 14:24:40 -0500

    scaffold basic plugin with shortcode template, style sheet, and script

---
+++ ammonite-particles-header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name:       Ammonite Particles Header
+ * Description:       Adds header with particles.js background
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
+
+/*
+  Basic Security
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+  die;
+}
+
+/*
+  Plugin Base Class
+*/
+if ( !class_exists( 'Ammonite_Particles_Header' ) ) {
+  class Ammonite_Particles_Header {
+    public static function register_styles_and_scripts() {
+      add_action( 'wp_enqueue_scripts', function() {
+        // Remove jQuery dependency if unnecessary
+        wp_register_script( 'ammonite-particles-header-script', plugins_url('assets/js/ammonite-particles-header.js', __FILE__ ), array('jquery'), '1.0.0', true );
+        wp_register_style( 'ammonite-particles-header-styles', plugins_url('assets/css/ammonite-particles-header.css', __FILE__ ), array(), '1.0.0', 'all' );
+      } );
+    }
+
+    public static function add_shortcode() {
+      // Use [ammonite_particles_header] to call this shortcode
+      add_shortcode( 'ammonite_particles_header', function( $atts ) {
+        // Handle shortcode attributes
+        $atts = shortcode_atts( array( 'header_text' => '' ), $atts );
+
+        // Enqueue styles and scripts wherever shortcode is called
+        wp_enqueue_script( 'ammonite-particles-header-script' );
+        wp_enqueue_style( 'ammonite-particles-header-styles' );
+
+        // Return template
+        ob_start();
+        include( 'templates/ammonite-particles-header-shortcode-template.php' );
+        return ob_get_clean();
+      } );
+    }
+  }
+
+  // Call methods on load here
+  Ammonite_Particles_Header::register_styles_and_scripts();
+  Ammonite_Particles_Header::add_shortcode();
+}
---
+++ templates/ammonite-particles-header-shortcode-template.php
@@ -0,0 +1,1 @@
+<span><?php echo $atts['header_text']; ?></span>
